bc9f9556d21f024c70eea9e062ac037ac72023c3
abmamun504@gmail.com <abmamun504@gmail.com>
2023-11-25 14:44:40 -0500

    created objects and invoked methods

--- script.js
+++ script.js
@@ -38,3 +38,27 @@ class Car {
     }
 }
 
+
+// Create car objects
+const honda = new Car('Honda', 'CR-V', 2023, 'Red', 50000, 45);
+const ford = new Car('Ford', 'F-150', 2020, 'Black', 25000, 30);
+const bmw = new Car('BMW', 'X5', 2022, 'Green', 60000, 65);
+const mazda = new Car('Mazda', 'CX-5', 2019, 'White', 15000, 60);
+const audi = new Car('Audi', 'Q7', 2018, 'Silver', 52000, 47);
+const kia = new Car('Kia', 'Forte', 2020, 'Blue', 21000, 56);
+
+// Invoke honk method for each car
+honda.honk();
+ford.honk();
+bmw.honk();
+mazda.honk();
+audi.honk();
+kia.honk();
+
+// Simulate race for 7 turns
+honda.race(7);
+ford.race(7);
+bmw.race(7);
+mazda.race(7);
+audi.race(7);
+kia.race(7);
